3a19d01ee445a51458add9eec2061068549257c6
RahulKujur09 <46575605+RahulKujur09@users.noreply.github.com>
2022-08-10 22:50:13 +0530

    Add files via upload
    The Above File is for testing purposes only.

---
+++ _My Jupyter Notebook on IBM Watson Studio_.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # "My Jupyter Notebook on IBM Watson Studio"
+
+# Hii there my name is Rahul Kujur. At present I'm working as a Rural Management professional. I'm looking forward forward to excel my career with the help of data science.
+
+# I'm interested in data science because, data is a gold of new age. And there must enough experts to explore and exploite all the potientials of data.
+
+# Here is a simple example of addition in Python
+
+# In[1]:
+
+
+1+1
+
+
+# https://www.youtube.com/
